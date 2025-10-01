d58d5f8e93e036dd28151685186921326b752817
unknown <zdravkodelic1993@gmail.com>
2025-10-01 12:27:12 +0200

    Fix: media query breakpoints(screen sizes); fix headers minimum width for the main game content width consistency

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ import Button from "./Button";
 export default function Header({ onStartGame, showStartbutton, label }) {
   return (
     <header className={classes.header}>
-      <h1>{"React Memory Game - Can you pair 'em all?"}</h1>
+      <h1>{"React Memory Game"}</h1>
+      <h2>{"Can you pair 'em all?"}</h2>
       <div className={classes["button-container"]}>
         {showStartbutton && <Button onClick={onStartGame}>{label}</Button>}
       </div>
