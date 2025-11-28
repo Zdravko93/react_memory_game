6a0910deda9ded59b93bb5ebd378f7462418b54b
unknown <zdravkodelic1993@gmail.com>
2025-11-28 21:31:50 +0100

    perf: change images import inside cardImages for performance

--- src/data/cardImages.js
+++ src/data/cardImages.js
@@ -1,15 +1,8 @@
-import angularLogo from "../assets/angular.webp";
-import cssLogo from "../assets/css.webp";
-import html5Logo from "../assets/html5.webp";
-import javaScriptLogo from "../assets/javaScript.webp";
-import reactLogo from "../assets/react.webp";
-import vueLogo from "../assets/vue.webp";
-
 export const cardImagePaths = [
-  angularLogo,
-  cssLogo,
-  html5Logo,
-  javaScriptLogo,
-  reactLogo,
-  vueLogo,
+  "../assets/angular.webp",
+  "../assets/css.webp",
+  "../assets/html5.webp",
+  "../assets/javaScript.webp",
+  "../assets/react.webp",
+  "../assets/vue.webp",
 ];
